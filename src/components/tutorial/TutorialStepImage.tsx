fe6a4c60f3cf7e322df7af08f6462e55518d93f4
Jacob <jacoborsbornsmith@gmail.com>
2026-02-20 17:14:35 +0000

    Tutorial: use plain img for step screenshots to fix Next.js image optimizer 400

--- src/components/tutorial/TutorialStepImage.tsx
+++ src/components/tutorial/TutorialStepImage.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import ExpandableImage from "@/components/ui/ExpandableImage";
-
 type TutorialStepImageProps = {
   src: string;
   alt: string;
 };
 
 export default function TutorialStepImage({ src, alt }: TutorialStepImageProps) {
-  return <ExpandableImage src={src} alt={alt} />;
+  return (
+    <div className="relative w-full overflow-hidden rounded-xl border bg-muted/30">
+      <img
+        src={src}
+        alt={alt}
+        loading="lazy"
+        style={{ width: "100%", height: "auto", borderRadius: 12 }}
+      />
+    </div>
+  );
 }
