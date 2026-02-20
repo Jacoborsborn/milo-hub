519b689ed045be7f9c3aad1f115deb3d85ef2cc8
Jacob <jacoborsbornsmith@gmail.com>
2026-02-20 20:18:50 +0000

    Meta Pixel: use pixel ID 1621407119051718

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   t.src=v;s=b.getElementsByTagName(e)[0];
   s.parentNode.insertBefore(t,s)}(window, document,'script',
   'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '${process.env.NEXT_PUBLIC_META_PIXEL_ID ?? ""}');
+  fbq('init', '1621407119051718');
   fbq('track', 'PageView');
 `}
         </Script>
